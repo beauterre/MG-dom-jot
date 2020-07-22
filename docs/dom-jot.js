6621e46e4fee62a47b5e0ccd4e381ab1455790f9
HjalmarSnoep <26629952+HjalmarSnoep@users.noreply.github.com>
2020-07-22 15:52:12 +0200

    Update dom-jot.js

--- docs/dom-jot.js
+++ docs/dom-jot.js
@@ -1,11 +1,17 @@
 document.addEventListener("load",init);
 function init()
 {
- console.log("init called");
-  document.body.addEventListener("resize",resize);
-  resize();
-}
-function resize()
-{
- console.log("resize called");
+  console.log("init called");
+  var ro = new ResizeObserver( entries => 
+  {
+     for (let entry of entries) 
+     {
+        const cr = entry.contentRect;
+        console.log('Element:', entry.target);
+        console.log(`Element size: ${cr.width}px x ${cr.height}px`);
+        console.log(`Element padding: ${cr.top}px ; ${cr.left}px`);
+     }
+  });
+  // Observe one or multiple elements
+  ro.observe(document.body);
 }
