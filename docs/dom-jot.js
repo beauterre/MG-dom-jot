bda74f36e05befec577a2570556f6349f1c34b4b
HjalmarSnoep <26629952+HjalmarSnoep@users.noreply.github.com>
2020-07-22 16:01:52 +0200

    Add files via upload

--- docs/dom-jot.js
+++ docs/dom-jot.js
@@ -1,15 +1,24 @@
-document.addEventListener("load",init);
+console.log("dom-jot script loaded..");
+const game={};
+init();
 function init()
 {
+	game.back=document.getElementById("table");
   console.log("init called");
   var ro = new ResizeObserver( entries => 
   {
      for (let entry of entries) 
      {
         const cr = entry.contentRect;
-        console.log('Element:', entry.target);
-        console.log(`Element size: ${cr.width}px x ${cr.height}px`);
-        console.log(`Element padding: ${cr.top}px ; ${cr.left}px`);
+		var w=cr.width;
+		var h=cr.height;
+		if((w/h)<(1280/1090)) h=w*1090/1280;
+		else w=h*1280/1090;
+		game.back.width=w;
+		game.back.height=h;
+		game.back.style.position="fixed";
+		game.back.style.top=((cr.height-h)/2)+"px";
+		game.back.style.left=((cr.width-w)/2)+"px";
      }
   });
   // Observe one or multiple elements
